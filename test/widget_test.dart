96965390b9e26e5faecc732642d3bcba85cff4ba
quangphuc123 <0306201369@caothang.edu.vn>
2022-11-01 21:13:58 +0700

    bỏ màn hình gamescreen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/gamescreen.dart';
 import 'package:flutter_application_1/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
