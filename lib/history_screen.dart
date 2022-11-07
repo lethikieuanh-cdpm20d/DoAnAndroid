2b2d4ced4549deff6be08ae1806373c1079a49f1
quangphuc123 <0306201369@caothang.edu.vn>
2022-11-08 01:46:25 +0700

    update history

--- lib/history_screen.dart
+++ lib/history_screen.dart
@@ -13,9 +13,43 @@ class HistoryScreen extends StatelessWidget {
             color: Colors.white,
           ),
         ),
+        backgroundColor: Color.fromARGB(255, 17, 224, 93),
       ),
       body: Container(
-        child: Column(),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                'Câu số 1:',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              subtitle: Text(
+                'Câu trả lời',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 17, 234, 76),
+                  minimumSize: const Size(200, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: Text('BACK'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
