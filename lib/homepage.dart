60c761bf561da59f48abbed20fa41b886f5e08be
lethikieuanh-cdpm20d <0306201310@caothang.edu.vn>
2022-12-13 23:50:24 +0700

    profile

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/listfriend.dart';
-import 'package:flutter_application_1/login.dart';
-import 'package:flutter_application_1/personal.dart';
-import 'package:flutter_application_1/rank.dart';
-import 'package:flutter_application_1/search.dart';
 
+import 'package:flutter_application_1/login.dart';
+import 'package:flutter_application_1/profile/profile_screen.dart';
+
+import 'package:flutter_application_1/search.dart';
+import 'package:flutter_application_1/signup.dart';
+import 'package:flutter_application_1/start.dart';
+import 'login.dart';
+import 'category_list/category.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 
 class homepageScreeen extends StatelessWidget {
@@ -19,13 +22,14 @@ class homepageScreeen extends StatelessWidget {
 }
 
 class homepageScreenHome extends StatefulWidget {
+  const homepageScreenHome ({Key? key}): super(key: key);
+  
   @override
-  State<StatefulWidget> createState() {
-    return homepageScreenState();
-  }
+  State<StatefulWidget> createState() => homepageScreenState();
 }
 
 class homepageScreenState extends State<homepageScreenHome> {
+  final user = FirebaseAuth.instance.currentUser!;
   int selectedIndex = 1;
   final Widget _myHome = MyHome();
   final Widget _myCategorys = MyCategorys();
@@ -49,7 +53,6 @@ class homepageScreenState extends State<homepageScreenHome> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-
         onWillPop: () async {
           final shouldPop = await showDialog<bool>(
             context: context,
@@ -72,128 +75,6 @@ class homepageScreenState extends State<homepageScreenHome> {
                     child: const Text('No'),
                   ),
                 ],
-
-      onWillPop: ()async{
-        final shouldPop = await showDialog<bool>(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: const Text('Thoát khỏi ứng dụng?'),
-              actionsAlignment: MainAxisAlignment.spaceBetween,
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context, true);
-                  },
-                  child: const Text('Yes'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context, false);
-                  },
-                  child: const Text('No'),
-                ),
-              ],
-            );
-          },
-        );
-        return shouldPop!;
-      },
-      child: Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children:[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 17, 224, 93),
-              ),
-              child: CircleAvatar(
-              child: Text(
-                'HN',
-                style:
-                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-              ),
-            ),
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.person,
-                color: Colors.green,
-              ),
-              title: const Text('Thông tin tài khoản'),
-              onTap: () {
-                //Navigator.pop(context);
-                 Navigator.push(context,
-                     MaterialPageRoute(builder: (context) => personalScreen()));
-              },
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.person,
-                color: Colors.blue,
-              ),
-              title: const Text('Bạn bè'),
-              onTap: () {
-                 Navigator.push(context,
-                     MaterialPageRoute(builder: (context) => ListFriend()));
-              },
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.insert_chart_outlined,
-                color: Colors.orange,
-              ),
-              title: const Text('Bảng xếp hạng'),
-              onTap: () {
-              Navigator.push(context,
-                     MaterialPageRoute(builder: (context) => rankSreen()));
-              },
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.map,
-                color: Colors.red,
-              ),
-              title: const Text('Checking'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.camera,
-                color: Colors.lightBlue,
-              ),
-              title: const Text('Camera'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-             ListTile(
-              leading: const Icon(
-                Icons.output_rounded,
-                color: Colors.red,
-              ),
-              title: const Text('Đăng xuất',style: TextStyle(color: Colors.red),),
-              onTap: () {
-                showDialog(context: context, builder: (context){
-                  return AlertDialog(
-                    title: const Text('Đăng xuất khỏi ứng dụng?'),
-                    icon: Icon(Icons.notifications),
-              actionsAlignment: MainAxisAlignment.spaceBetween,
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context, true);
-                    Navigator.pushAndRemoveUntil<void>(
-                context,
-                MaterialPageRoute<void>(
-                  // ignore: prefer_const_constructors
-                  builder: (BuildContext context) => LoginScreen(),
-                ),
-                (Route<dynamic> route) => false,
-              ]
               );
             },
           );
@@ -227,7 +108,7 @@ class homepageScreenState extends State<homepageScreenHome> {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => personalScreen()));
+                            builder: (context) => ProfileScreen()));
                   },
                 ),
                 ListTile(
@@ -354,8 +235,8 @@ class homepageScreenState extends State<homepageScreenHome> {
               }),
         ));
   }
+}
 
-        }}}
 class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -373,7 +254,6 @@ class MyCategorys extends StatelessWidget {
 class Challenge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Coming Soon"));
+    return signupScreen();
   }
 }
-      
---
+++ lib/profile/edit.dart
@@ -0,0 +1,215 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/profile/UserObject.dart';
+import 'package:flutter_application_1/profile/profile_screen.dart';
+
+class EditprofileScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return EditprofileScreenState();
+  }
+}
+
+class EditprofileScreenState extends State<EditprofileScreen> {
+  TextEditingController txtname = TextEditingController();
+  TextEditingController txtpass = TextEditingController();
+  List<UserObject> lsUser = [];
+
+  final ref = FirebaseDatabase.instance.ref();
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user != null) {
+        uidUser = user.uid;
+      }
+    });
+    userData(); // kt va hien thi thong tin
+    setState(() {});
+  }
+
+  void userData() {
+    ref.child("users").onChildAdded.listen((data) {
+      UserObject userObject = UserObject.fromJson(data.snapshot.value as Map);
+      lsUser.add(userObject);
+      setState(() {});
+    });
+  }
+
+  Mail() {
+    for (int i = 0; i < lsUser.length; i++) {
+      if (lsUser[i].uid == uidUser) {
+        return '${lsUser[i].email}';
+      }
+    }
+    return 'khong co';
+  }
+
+  Ten() {
+    for (int i = 0; i < lsUser.length; i++) {
+      if (lsUser[i].uid == uidUser) {
+        return '${lsUser[i].name}';
+      }
+    }
+    return 'khong co';
+  }
+
+  Pass() {
+    for (int i = 0; i < lsUser.length; i++) {
+      if (lsUser[i].uid == uidUser) {
+        return '${lsUser[i].password}';
+      }
+    }
+  }
+
+  String uidUser = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 169, 237, 181),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 127, 242, 101),
+        title: const Text(
+          'Chỉnh sửa thông tin',
+          style: TextStyle(fontSize: 25),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: 1,
+        itemBuilder: (context, index) => Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 40),
+              child: ClipOval(
+                child: Image.asset(
+                  'images/anh.png',
+                  width: 140,
+                  height: 140,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20, bottom: 20),
+              //padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  const Text(
+                    'NAME',
+                    style: TextStyle(
+                        fontSize: 25, color: Color.fromARGB(255, 229, 13, 204)),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 50, right: 50, top: 5),
+                    child: TextField(
+                      controller: txtname,
+                      decoration: InputDecoration(
+                        label: Text(
+                          "   ${Ten()}",
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.pink),
+                        ),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 172, 240, 172),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        prefixIcon: const Icon(Icons.person),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              // padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  const Text(
+                    'Email',
+                    style: TextStyle(
+                        fontSize: 25, color: Color.fromARGB(255, 187, 16, 90)),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 50, right: 50, top: 5),
+                    child: Text(
+                      '${Mail()}',
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Color.fromARGB(255, 240, 7, 127)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 50,
+              margin: const EdgeInsets.only(top: 50),
+              //padding: EdgeInsets.all(15),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: const MaterialStatePropertyAll<Color>(
+                      Color.fromARGB(255, 44, 6, 232)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+                child: const Text('Lưu', style: TextStyle(fontSize: 28)),
+                onPressed: () {
+                  if (txtname.text != "") {
+                    FirebaseAuth auth = FirebaseAuth.instance;
+                    final ref = FirebaseDatabase.instance.ref().child('users');
+                    for (int i = 0; i < lsUser.length; i++) {
+                      if (uidUser == lsUser[i].uid) {
+                        ref.child(lsUser[i].uid).update({
+                          'name': txtname.text,
+                        });
+                      }
+                      else if (txtname.text == '')
+                    {
+                         showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text(
+                              'THÔNG BÁO',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            content: Text('Mời nhập tên <3'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        });
+                    }
+                    } 
+                    
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfileScreen()));
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
